1d3da9b093847340f4fade42da642aa8435eff97
Javier Godoy <jack@gaulatti.com>
2026-05-05 13:51:54 -0400

    fix: Increase upload limit to 100MB

--- src/test/java/com/gaulatti/colombo/ColomboApplicationTests.java
+++ src/test/java/com/gaulatti/colombo/ColomboApplicationTests.java
@@ -1,13 +1,27 @@
 package com.gaulatti.colombo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.servlet.autoconfigure.MultipartProperties;
+import org.springframework.util.unit.DataSize;
 
 @SpringBootTest
 class ColomboApplicationTests {
 
+    @Autowired
+    private MultipartProperties multipartProperties;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void multipartUploadLimitIsOneHundredMegabytes() {
+        assertEquals(DataSize.ofMegabytes(100), multipartProperties.getMaxFileSize());
+        assertEquals(DataSize.ofMegabytes(100), multipartProperties.getMaxRequestSize());
     }
 
 }
